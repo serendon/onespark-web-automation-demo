6c32ef067ee26893130b8ea36d348cb4102a8512
Serendon Naicker <serendonnaicker@Serendons-MacBook-Pro.local>
2024-02-18 12:24:05 +0200

    add selenium driver helper class

---
+++ src/test/java/helpers/HelperClass.java
@@ -0,0 +1,52 @@
+package helpers;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+
+public class HelperClass {
+
+    private static HelperClass helperClass;
+    private static WebDriver driver;
+    public final static int TIMEOUT = 5;
+
+    private HelperClass() {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
+    }
+
+    public static void openPage(String url) {
+        driver.get(url);
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void setUpDriver() {
+
+        if (helperClass==null) {
+
+            helperClass = new HelperClass();
+        }
+    }
+
+    @AfterTest
+    public static void tearDown() {
+
+        if(driver!=null) {
+            driver.close();
+            driver.quit();
+        }
+
+        helperClass = null;
+    }
+
+}
